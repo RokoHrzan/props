ea72c39be37c07da030780a17362bbee10824f25
hyperv <hyperv@ucione.local>
2022-03-25 20:05:54 +0100

    App init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './App.css';
+const users = [
+  {name: "Pero", age: 21},
+  {name: "Ana", age:22},
+  {name:"Mirko", age: 41}
+]
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <></>
+    );
+  }
+}
